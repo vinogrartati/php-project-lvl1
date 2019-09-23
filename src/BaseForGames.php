df55197579ed70c398fa6056239d4125920f87e7
Vinogradova Tatiana <vinogradova@vl.ru>
2019-09-23 14:32:04 +1000

    Refactoring

---
+++ src/BaseForGames.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BrainGames\BaseForGames;
+
+use function \cli\line;
+use function \cli\prompt;
+
+function game($instruction, $questionCreate, $answer)
+{
+    $question = [];
+    line('Welcome to the Brain Game!');
+    line('%s', $instruction);
+    line('');
+    $playerName = prompt('May I have your name?');
+    line("Hello, %s!", $playerName);
+    line('');
+    for ($i = 0; $i < 3; $i++) {
+            $question[$i] = $questionCreate();
+            $correctAnswer = $answer($question[$i]);
+            line('Question:%s', $question[$i]);
+            $playerAnswer = prompt('Your answer');
+        if ($correctAnswer == $playerAnswer) {
+                line('Correct!');
+            if ($i == 2) {
+                    line('Congratulations, %s!', $playerName);
+            }
+        } else {
+                line('\'%s\' is wrong answer ;(. Correct answer was \'%s\'', $playerAnswer, $correctAnswer);
+                line('Let\'s try again, %s!', $playerName);
+                break;
+        }
+    }
+}
--- src/games/CalculationGame.php
+++ src/games/CalculationGame.php
@@ -1,56 +1,32 @@
 <?php
 
-namespace BrainGames\CalculationGame;
+namespace BrainGames\Games\CalculationGame;
 
 use function \cli\line;
 use function \cli\prompt;
+use function BrainGames\BaseForGames\game;
 
-function instruction()
+
+function calculate()
 {
-    return "What is the result of the expression?";
-}
-function getQuestion()
-{
-    $value1 = rand(1, 100);
-    $value2 = rand(1, 100);
-    $array = ["{$value1} * {$value2}", "{$value1} + {$value2}", "{$value1} - {$value2}"];
-    $key = array_rand($array, 1);
-    return $array[$key];
-}
-function correctAnswer($question)
-{
-    $array = explode(" ", $question);
-    if ($array[1] == "*") {
-        return $array[0] * $array[2];
-    } elseif ($array[1] == "+") {
-        return $array[0] + $array[2];
-    } else {
-        return $array[0] - $array[2];
-    }
-}
-function game()
-{
-    $question = [];
-    line('Welcome to the Brain Game!');
-    line('%s', instruction());
-    line('');
-    $playerName = prompt('May I have your name?');
-    line("Hello, %s!", $playerName);
-    line('');
-    for ($i = 0; $i < 3; $i++) {
-            $question[$i] = getQuestion();
-            $correctAnswer = correctAnswer($question[$i]);
-            line('Question:%s', $question[$i]);
-            $playerAnswer = prompt('Your answer');
-        if ($correctAnswer == $playerAnswer) {
-                line('Correct!');
-            if ($i == 2) {
-                    line('Congratulations, %s!', $playerName);
+    return game(
+        'What is the result of the expression?',
+        function () {
+            $value1 = rand(0, 100);
+            $value2 = rand(0, 100);
+            $array = ["{$value1} * {$value2}", "{$value1} + {$value2}", "{$value1} - {$value2}"];
+            $key = array_rand($array, 1);
+            return $array[$key];
+        },
+        function ($question) {
+            $array = explode(" ", $question);
+            if ($array[1] == "*") {
+                return $array[0] * $array[2];
+            } elseif ($array[1] == "+") {
+                return $array[0] + $array[2];
+            } else {
+                return $array[0] - $array[2];
             }
-        } else {
-                line('\'%s\' is wrong answer ;(. Correct answer was \'%s\'', $playerAnswer, $correctAnswer);
-                line('Let\'s try again, %s!', $playerName);
-                break;
         }
-    }
+    );
 }
